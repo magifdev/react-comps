571a55d906c1f59b0dfe3600be7c93c49fd1d98b
magifdev <marcgibert91@gmail.com>
2022-11-28 13:27:24 +0100

    validation with prop-type

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App() {
   return (
     <div>
       <div>
-        <Button></Button>
+        <Button success rounded outline>Click me!!</Button>
       </div>
       <div>
-        <Button></Button>
+        <Button sanged outline>Buy now!</Button>
       </div>
       <div>
-        <Button></Button>
+        <Button warning>See Deal</Button>
       </div>
       <div>
-        <Button></Button>
+        <Button secondary outline>Hide Ads</Button>
       </div>
       <div>
-        <Button></Button>
+        <Button secondary rounded>Sell that</Button>
       </div>
     </div>
   );

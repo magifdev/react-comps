b27808112484a54596d194ce9ffa3eb0540e3d15
magifdev <marcgibert91@gmail.com>
2022-11-29 13:24:24 +0100

    event handler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from "./Button";
 
 function App() {
+  const handleClick = () => {
+    console.log('click!');
+  };
+
   return (
     <div>
       <div>
-        <Button success rounded outline>Click me!!</Button>
+        <Button secondary outline rounded className="mb-5" onClick={handleClick} >
+          <GoBell />
+          Click me!!
+        </Button>
       </div>
       <div>
-        <Button danger outline>Buy now!</Button>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy now!
+        </Button>
       </div>
       <div>
-        <Button warning>See Deal</Button>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deal
+        </Button>
       </div>
       <div>
-        <Button secondary outline>Hide Ads</Button>
+        <Button secondary outline>
+          Hide Ads
+        </Button>
       </div>
       <div>
-        <Button primary rounded>Sell that</Button>
+        <Button primary rounded>
+          Sell that
+        </Button>
       </div>
     </div>
   );
